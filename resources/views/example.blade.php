0817a9040d84f2f5e3f214f7ddfd14dfb6d58625
Nicolas Rossitto <49282@etu.he2b.be>
2021-10-15 16:47:26 +0200

    ajout vue et authantification

---
+++ resources/views/example.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section class="related-post-section spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-title">
+                    <h2>Home</h2>
+                    <p class="mt-3">La vue est entrain d'être faite!</p>
+                    <p> Nom : Rossitto</p>
+                    <p> Prénom : Nicolas</p>
+                    <p> Matricule : 49282</p>
+                    <p> Adresse email : 49282@etu.he2b.be </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
