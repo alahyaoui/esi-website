25f37bf80206d6df4262049a714f91019dcc0bf1
Amine-Ayoub Bigham <54985@etu.he2b.be>
2021-11-09 08:46:35 +0100

    add home redirect

--- resources/views/incs/header.blade.php
+++ resources/views/incs/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Home</a>
+    <a class="navbar-brand" href="/">Home</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -23,7 +23,7 @@
     @endauth
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
             <a class="nav-item nav-link" href="programme">Programme</a>
             <a class="nav-item nav-link" href="pae">PAE</a>
             <a class="nav-item nav-link" href="cavp">CAVP</a>
