1f08a5eb5f47133208b5ce58c44f7c97d470c4ae
Amine-Ayoub Bigham <54985@etu.he2b.be>
2021-11-23 12:28:53 +0100

    header affichage

--- resources/views/incs/header.blade.php
+++ resources/views/incs/header.blade.php
@@ -1,33 +1,37 @@
 <nav class="navbar navbar-dark bg-dark">
     <a class="navbar-brand" href="/">Home</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     @auth
-    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:white;" v-pre>
-        {{ Auth::user()->email }} <span class="caret"></span>
-    </a>
-
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                           document.getElementById('logout-form').submit();">
-            {{ __('Logout') }}
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+           aria-haspopup="true" aria-expanded="false" style="color:white;" v-pre>
+            {{ Auth::user()->email }} <span class="caret"></span>
         </a>
 
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-        </form>
-    </div>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                           document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+        </div>
     @endauth
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>     
-            @if(Auth::User()->is_student)  
+            <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
+            @if(Auth::User()->is_student)
                 <a class="nav-item nav-link" href="programme">Programme</a>
                 <a class="nav-item nav-link" href="pae">PAE</a>
                 <a class="nav-item nav-link" href="cavp">CAVP</a>
             @endif
-            <a class="nav-item nav-link" href="studentregister">S'inscrire à l'ESI</a>
+            @if(!Auth::User()->is_student)
+                <a class="nav-item nav-link" href="studentregister">S'inscrire à l'ESI</a>
+            @endif
         </div>
     </div>
 
