30d26944373a0210f6ffdbc250aeb0c9db622c40
Ben Kaddour Ismael 54966 <G54966@app.pdg.intranet.esi-bru.be>
2021-12-07 11:18:41 +0100

    Fix errors in Course class.

--- app/Business/Course.php
+++ app/Business/Course.php
@@ -8,25 +8,25 @@ class Course {
     private $prerequis;
     private $corequis;
     
-
-    public function __construct($title){
-        $this->$title = $title;
-        $this->$prerequis = array();
-        $this->$corequis = array();
+    //Functions
+    public function __construct($course_title){
+        $this->title = $course_title;
+        $this->prerequis = array();
+        $this->corequis = array();
     }
     /**
      * Add a prerequis to the course
      */
     public function add_prerequis($course){
-        array_push($this->$prerequis,$course);
+        array_push($this->prerequis, $course);
     }
     public function getPrerequis(){
-        return $this->$prerequis;
+        return $this->prerequis;
     }
     /**
      * Add a corequis to the course
      */
     public function add_corequis($course){
-        array_push($this->$corequis,$course);
+        array_push($this->corequis, $course);
     }   
 }
