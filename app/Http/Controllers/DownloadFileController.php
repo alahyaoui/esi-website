7967781663152c84ac8c709269cb9fa9b2a27cc3
Amine-Ayoub Bigham <54985@etu.he2b.be>
2021-12-14 09:30:07 +0100

    Download Controller doc

--- app/Http/Controllers/DownloadFileController.php
+++ app/Http/Controllers/DownloadFileController.php
@@ -2,15 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Response;
 
 class DownloadFileController extends Controller
 {
+    /**
+     * Permet de télécharger un fichier.
+     *
+     * @param $file_path
+     * @return Response
+     * @throws FileNotFoundException
+     */
     public function index($file_path)
     {
-        $file=Storage::disk('public')->get($file_path);
-		return (new Response($file, 200));
+        $file = Storage::disk('public')->get($file_path);
+        return (new Response($file, 200));
     }
 }
