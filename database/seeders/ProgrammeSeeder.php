7ac48a8a24ba69f74a1b40d2f8c1259261b77d78
ayoub lahyaoui <ayoub.lahyaoui@hotmail.com>
2021-12-19 18:05:56 +0100

    Update Student Seeder

--- database/seeders/ProgrammeSeeder.php
+++ database/seeders/ProgrammeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Http\Controllers\ProgrammeController;
 use App\Models\Course;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -16,38 +17,7 @@ class ProgrammeSeeder extends Seeder
      */
     public function run()
     {
-
-        $courses = DB::select('select * from course_section where section like \'G%\'');
-        if ($courses != null) {
-            foreach ($courses as $course) {
-                $rand = rand(0, 1);
-                DB::table('programme')->insert([
-                    'student' => 54247,
-                    'course' => $course->course,
-                    'is_validated' => $rand == 1,
-                    'cote' => $rand == 1 ? rand(0, 20) : 0
-                ]);
-            }
-        }
-        DB::table('programme')->insert([
-            'student' => 54985,
-            'course' => "ANA2",
-            'is_validated' => true,
-            'is_accessible' => true,
-            'cote' =>  12
-        ]);
-        DB::table('programme')->insert([
-            'student' => 54985,
-            'course' => "DEV2",
-            'is_validated' => true,
-            'is_accessible' => true,
-            'cote' =>  16
-        ]);
-        DB::table('programme')->insert([
-            'student' => 54985,
-            'course' => "ANA3",
-            'is_validated' => false,
-            'cote' =>  0
-        ]);
+        ProgrammeController::initStudentBulletin("12345");
+        ProgrammeController::importStudentsBulletin("studentsBulletin");
     }
 }
